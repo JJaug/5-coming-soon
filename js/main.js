c90a17bd45825b619fb9fa5901b2dc6b307b918c
Jonas <jonas.jaugas@gmail.com>
2021-01-26 16:56:51 +0200

    renderSocial

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
-console.log('coming soon');
+import {socialsData} from './data/socialsData.js';
+import {renderSocials} from './components/renderSocials.js';
+
+renderSocials('footer > .socials',socialsData)

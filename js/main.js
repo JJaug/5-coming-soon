7231f4c64c3dc11ebd03c93fd7491abc379e77cf
Jonas <jonas.jaugas@gmail.com>
2021-02-01 10:30:14 +0200

    updated clock

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-import {clock} from './components/clock.js'
-import {socialsData} from './data/socialsData.js';
-import {renderSocials} from './components/renderSocials.js';
+import { clock } from './components/clock.js';
+import { socialsData } from './data/socialsData.js';
+import { renderSocials } from './components/renderSocials.js';
 
-
-clock('.clock');
-renderSocials('footer > .socials',socialsData);
-
+clock('.clock', '01-04 14:00:00');
+renderSocials('footer > .socials', socialsData);
